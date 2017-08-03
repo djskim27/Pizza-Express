044f55dee8a66d3dc3e681c816d61302be266cb9
djskim27 <djs.kim27@gmail.com>
2017-08-03 15:56:27 -0400

    Deploying with create-react-app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import locationData from './locationData';
+import Navbar from './components/Navbar';
+import SplashImage from './components/SplashImage';
+import Description from './components/Description';
+import MenuList from './components/MenuList';
+import LocationList from './components/LocationList';
+import Contact from './components/Contact';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <SplashImage />
+        <Description />
+        <MenuList />
+        <LocationList />
+        <Contact />
+      </div>
+    );
+  }
+}
+
+export default App;
